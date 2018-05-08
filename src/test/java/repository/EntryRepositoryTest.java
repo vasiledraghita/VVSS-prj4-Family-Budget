1d98dcb17fc58b618fdb0d9f808cdeb9e606392e
Vasile Draghita <vasiledraghita@gmail.com>
2018-05-08 18:56:52 +0300

    Added integration tests

--- src/test/java/repository/EntryRepositoryTest.java
+++ src/test/java/repository/EntryRepositoryTest.java
@@ -1,7 +1,9 @@
 package repository;
 
+import exceptions.InvalidNameException;
 import exceptions.InvalidTypeException;
 import exceptions.MemberNotExistsException;
+import model.Member;
 import org.junit.Before;
 import org.junit.Test;
 import model.Entry;
@@ -37,5 +39,42 @@ public class EntryRepositoryTest {
         }
         assert(eRepo.getAllEntries().get(9).getIdMember()==1);
         assert("income".equals(eRepo.getAllEntries().get(9).getType()));
+    }
+    @Test
+    public void integrationTest1() {
+        MemberRepository mRepo = new MemberRepository();
+
+        Member member = new Member("testName", 4);
+        try {
+            mRepo.addMember(member);
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+        assert(mRepo.getAllMembers().get(3).getName().equals(member.getName()));
+    }
+    @Test
+    public void integrationTest2() {
+        integrationTest1();
+
+        MemberRepository mRepo = new MemberRepository();
+        EntryRepository eRepo = new EntryRepository(mRepo);
+
+        Entry entry = new Entry("income", 100, 4);
+        try {
+            eRepo.addEntry(entry);
+        } catch (InvalidTypeException | MemberNotExistsException e) {
+            e.printStackTrace();
+        }
+        assert (eRepo.getAllEntries().get(9).getIdMember() == 4);
+    }
+    @Test
+    public void integrationTest3() throws MemberNotExistsException {
+        integrationTest1();
+        integrationTest2();
+
+        MemberRepository mRepo = new MemberRepository();
+        EntryRepository eRepo = new EntryRepository(mRepo);
+
+        assert (eRepo.getEntryForOneMember(4).get(0).getValue() == 100);
     }
 }
